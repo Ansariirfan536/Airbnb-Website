e0639e192d7725cb35c9c6e3753e684c0cb6f08c
IrfanAnsari <irfanansariji7072@gmail.com>
2026-06-08 13:19:36 +0530

    Added professional security verification modal to show page

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,6 +1,6 @@
 const Listing = require("../models/listing");
 
-// simple alias map for common country synonyms (module-level so it's always defined)
+// Alias map for search
 const aliases = {
   'england': 'United Kingdom',
   'uk': 'United Kingdom',
@@ -13,7 +13,6 @@ const aliases = {
 };
 
 module.exports.index = async (req, res) => {
-  // accept multiple possible query param names from the navbar or other clients
   const term = req.query.country || req.query.q || req.query.search;
   const filter = {};
   let searched = false;
@@ -34,10 +33,6 @@ module.exports.index = async (req, res) => {
       ];
     }
     const allListings = await Listing.find(filter);
-    // debug logging to help diagnose why no results appear
-    // console.log('Listing search term:', term);
-    // console.log('Mongo filter used:', JSON.stringify(filter));
-    // console.log('Results count:', allListings.length);
     res.render("listings/index.ejs", { allListings, country: term || '', searchCount: allListings.length, searched });
   } catch (err) {
     console.error('Error in listings.index:', err);
@@ -45,28 +40,25 @@ module.exports.index = async (req, res) => {
   }
 };
 
-
 module.exports.renderNewForm = (req, res) => {
   res.render("listings/new.ejs");
 };
+
 module.exports.showListing = async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id)
     .populate({
       path: "reviews",
-      populate: {
-        path: "author",
-      },
+      populate: { path: "author" },
     })
-    .populate("owner");
+    .populate("owner"); // Yeh owner populate hona zaroori hai button ke liye
+
   if (!listing) {
-    req.flash("error", "Sorry!!  does not exist your listing please recheck your listing & try again ");
-    res.redirect("/listings")
+    req.flash("error", "Listing does not exist!");
+    return res.redirect("/listings");
   }
-  //console.log(listing);
   res.render("listings/show.ejs", { listing });
 };
-
 
 module.exports.createListing = async (req, res, next) => {
   let url = req.file.path;
@@ -74,46 +66,41 @@ module.exports.createListing = async (req, res, next) => {
 
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
-  newListing.image = { url, filename }
+  newListing.image = { url, filename };
   await newListing.save();
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
 };
 
-
 module.exports.renderEditForm = async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   if (!listing) {
-    req.flash("error", "Sorry!!  does not exist your listing please recheck your listing & try again ");
-    res.redirect("/listings")
+    req.flash("error", "Listing does not exist!");
+    return res.redirect("/listings");
   }
-  let originalImageUrl = listing.image.url;
-  originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_100,w_150")
+  let originalImageUrl = listing.image.url.replace("/upload", "/upload/h_100,w_150");
   res.render("listings/edit.ejs", { listing, originalImageUrl });
-}
-
+};
 
 module.exports.updateListing = async (req, res) => {
   let { id } = req.params;
   let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
 
-  if (typeof req.file !== "undefined") {
+  // Agar user ne nayi photo upload ki hai
+  if (req.file) {
     let url = req.file.path;
     let filename = req.file.filename;
     listing.image = { url, filename };
     await listing.save();
   }
-  req.flash("success", "Updated Listing")
+  req.flash("success", "Listing Updated!");
   res.redirect(`/listings/${id}`);
-}
-
+};
 
 module.exports.destroyListing = async (req, res) => {
   let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  req.flash("success", "Delete Listing!!")
-  res.redirect("/listings")
-}
-
+  await Listing.findByIdAndDelete(id);
+  req.flash("success", "Listing Deleted!");
+  res.redirect("/listings");
+};
